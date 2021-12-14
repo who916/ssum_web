87734e7100ef5e7812440ccb2f4b46ca6298b562
09929786 <macchayang@gmail.com>
2021-12-14 17:12:00 +0900

    상세제어

--- ssum_web/src/main/resources/static/bootstrap/js/post/postDetail.js
+++ ssum_web/src/main/resources/static/bootstrap/js/post/postDetail.js
@@ -5,18 +5,4 @@ var commonUtil;
 $(document).ready(function () {
     commonUtil = commonUtil.prototype;
 
-	//탭 제어
-	$('.text-tab').click(function(){
-
-	   $('.text-tab').removeClass('current');
-	   $(this).addClass('current');
-
-	});
-
-	//paging tab제어
-	$('.pagination').click(function(){
-        $('.pagination').removeClass('select');
-        $(this).addClass('select');
-
-	});
 });
