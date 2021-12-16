30a857af96cef865640b80f6b46984c19beada25
09929786 <macchayang@gmail.com>
2021-12-16 16:24:34 +0900

    주소수정

--- ssum_web/src/main/java/com/kyobo/dev/ssum/web/Controller/ReadbookController.java
+++ ssum_web/src/main/java/com/kyobo/dev/ssum/web/Controller/ReadbookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReadbookController {
 	 @RequestMapping("/novel.do")
 	 public String getNovelPage() {
-		 return "/pages/read-book/novel";
+		 return "/pages/read-book/novelMain";
 	 }
 	 
 }
